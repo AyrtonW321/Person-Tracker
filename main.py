916212fb3f3d9c1a6203cef69c86c66d7f4220f3
Ayrton Wong <[ayrtonwongg@gmail.com]>
2026-01-31 20:28:05 -0500

    Create main file for initiation (camera functionality only)

---
+++ main.py
@@ -0,0 +1,38 @@
+
+''' 
+Stars camera
+Run loop
+handles quitting and cleanup
+''' 
+
+import cv2 as cv
+
+from vision.camera import Camera
+from vision.colour_tracker import ColourTracker
+from ui.overlay import draw_crosshair, draw_tracking_overlay
+
+
+def main():
+    camera = Camera()
+    tracker = ColourTracker()
+
+    try:
+        while True:
+            frame = camera.read()  # BGR
+            result, mask = tracker.process(frame)
+
+            draw_crosshair(frame)
+            draw_tracking_overlay(frame, result)
+
+            cv.imshow("Video", frame)
+            cv.imshow("Mask", mask)
+
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+    finally:
+        camera.close()
+        cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
